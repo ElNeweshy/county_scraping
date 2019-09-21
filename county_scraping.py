65c11965db40a97a72a95c0e8a686f911b1a115d
ElNeweshy <mahmoud.elneweshy@chestnut.com>
2019-09-21 00:23:00 -0700

    Update county_scraping.py

--- county_scraping.py
+++ county_scraping.py
@@ -7,7 +7,6 @@ import numpy as np
 import ast
 
 
-
 def read_inputs(csv_file):
     data = pd.read_csv(csv_file, header=None)
     from_date = str(data.iloc[0][1].replace('.', '-'))
@@ -24,6 +23,11 @@ def get_number_of_pages(from_date, to_date):
     number_of_results = eval(number_of_results_row.split('results')[0][:-1].replace(',', ''))
 
     number_of_pages = int(number_of_results / 15) + 1
+
+    print('Number of search results is:', number_of_results)
+    print('Number of pages is:', number_of_pages)
+
+    driver.close()
 
     return number_of_pages
 
@@ -59,53 +63,107 @@ def extract_data_from_page(page_link, row_index):
             'Returned_on': '',
             'Grantor': '',
             'Grantee': '',
-            'Legal Description': '',
+            'Legal_Description': '',
             }
 
     County = BeautifulSoup(str(tds[0]), 'html.parser').text
     dict.update({'County': County})
 
-    Instrument = BeautifulSoup(str(tds[1]), 'html.parser').text.replace('\n', '')
-    dict.update({'Instrument': Instrument})
+    try:
+        Instrument = BeautifulSoup(str(tds[1]), 'html.parser').text.replace('\n', '')
+        dict.update({'Instrument': Instrument})
+    except:
+        pass
 
-    Type = BeautifulSoup(str(tds[2]), 'html.parser').find('a').text
-    dict.update({'Type': Type})
+    try:
+        Type = BeautifulSoup(str(tds[2]), 'html.parser').find('a').text
+        dict.update({'Type': Type})
+    except:
+        pass
 
     Book = BeautifulSoup(str(tds[3]), 'html.parser').text
     dict.update({'Book': Book})
 
-    Page = BeautifulSoup(str(tds[3]), 'html.parser').text
-    dict.update({'Page': Page})
-
-    Recorded_on = BeautifulSoup(str(tds[7]), 'html.parser').text.replace('\n', '')
-    dict.update({'Recorded_on': Recorded_on})
+    Recorded = BeautifulSoup(str(tds[7]), 'html.parser').text.replace('\n', '')
+    dict.update({'Recorded': Recorded})
 
     People = BeautifulSoup(str(tds[5]), 'html.parser').text.replace('\n', '')
     dict.update({'People': People})
 
-    driver.get('https://okcountyrecords.com/detail/' + County + '/' + Instrument)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    if Instrument != '':
+
+        # Scrap secondary page
+        driver.get('https://okcountyrecords.com/detail/' + County + '/' + Instrument)
+
+        Filing_Fees = driver.find_element_by_xpath('//*[@id="detail-fees"]/table/tbody/tr[1]/td').text
+        dict.update({'Filing_Fees': Filing_Fees})
+
+        Mortgage_amount = driver.find_element_by_xpath('//*[@id="detail-fees"]/table/tbody/tr[2]/td').text
+        dict.update({'Mortgage_amount': Mortgage_amount})
+
+        Document_stamps = driver.find_element_by_xpath('//*[@id="detail-fees"]/table/tbody/tr[3]/td').text
+        dict.update({'Document_stamps': Document_stamps})
+
+        try:
+            Grantor = driver.find_element_by_xpath(
+                '// *[ @ id = "detail-people"] / ul / li[1] / ul / li').text.replace('Search', '').replace('\n', '')
+            dict.update({'Grantor': Grantor})
+        except:
+            pass
+
+        try:
+            Grantee = driver.find_element_by_xpath('//*[@id="detail-people"]/ul/li[2]/ul/li').text.replace('Search',
+                                                                                                                '').replace(
+                '\n', '')
+            dict.update({'Grantee': Grantee})
+        except:
+            pass
+
+        try:
+            Legal_Description = driver.find_element_by_xpath('//*[@id="detail-legals"]/ul/li').text.replace('Search',
+                                                                                                            '').replace(
+                '\n', '')
+            dict.update({'Legal_Description': Legal_Description})
+        except:
+            pass
+
+        Recorded_on = driver.find_element_by_xpath('//*[@id="detail-fees"]/table/tbody/tr[4]/td').text
+        dict.update({'Recorded_on': Recorded_on})
+
+        try:
+            Instrument_date = driver.find_element_by_xpath('//*[@id="detail-fees"]/table/tbody/tr[5]/td').text
+            dict.update({'Instrument_date': Instrument_date})
+        except:
+            pass
+
+        Returned_on = driver.find_element_by_xpath('//*[@id="secondary-details"]/table/tbody/tr[2]/td').text
+        dict.update({'Returned_on': Returned_on})
+
+        Page = driver.find_element_by_xpath('//*[@id="primary-details"]/table/tbody/tr[2]/td').text
+        dict.update({'Page': Page})
+
+    driver.close()
 
     return dict
 
 
 def create_csv_form_text_file(text_file, output_file_name):
     columns = ['Type',
-            'Book',
-            'Page',
-            'County',
-            'Instrument',
-            'Recorded',
-            'Filing_Fees',
-            'Mortgage_amount',
-            'Document_stamps',
-            'Recorded_on',
-            'Instrument_date',
-            'Returned_on',
-            'Grantor',
-            'Grantee',
-            'Legal Description'
-            ]
+               'Book',
+               'Page',
+               'County',
+               'Instrument',
+               'Recorded',
+               'Filing_Fees',
+               'Mortgage_amount',
+               'Document_stamps',
+               'Recorded_on',
+               'Instrument_date',
+               'Returned_on',
+               'Grantor',
+               'Grantee',
+               'Legal_Description'
+               ]
 
     dicts = open(text_file, 'r').readlines()
 
@@ -128,12 +186,17 @@ if __name__ == "__main__":
 
     dicts = []
     for page_number in range(number_of_pages):
-        print('Page: ', page_number + 1)
+        print('Page:', page_number + 1)
         page_link = get_page_link(inputs[0], inputs[1], page_number + 1)
         print('Page link: ', page_link)
+
         for row_index in range(15):
-            print('Row: ', row_index + 1)
-            dict = extract_data_from_page(page_link, row_index + 1)
+            print('Row:', row_index + 1)
+            try:
+                dict = extract_data_from_page(page_link, row_index + 1)
+            except:
+                continue
+
             print(dict)
 
             with open('output.txt', 'a') as output_text_file:
